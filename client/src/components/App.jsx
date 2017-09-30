2054953da2e1e28baec45bc8a952c825e0e39873
amatranga <amatranga892@gmail.com>
2017-09-30 15:41:16 -0700

    Update App component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import Navbar from './Navbar';
+import InputForm from './InputForm';
+import Results from './Results';
 
-const App = () => {
-  return (
-    <div>
-      HI SIMON!!!
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: ''
+    }
+
+    this.submitAddress = this.submitAddress.bind(this);
+  }
+
+  submitAddress(address) {
+    event.preventDefault();
+    this.setState({address})
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <InputForm submitAddress={this.submitAddress}/>
+        <hr />
+        <Results />
+      </div>
+    );
+  }
 }
 
 export default App;
