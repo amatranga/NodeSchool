b91e78a02704884894fc27d10b0524fe8175ddca
amatranga <amatranga892@gmail.com>
2017-09-30 15:42:14 -0700

    Implements simple Results component

---
+++ client/src/components/Results.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        Results go here
+      </div>
+    );
+  }
+}
+
+export default Results;
