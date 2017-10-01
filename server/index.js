74f8a3b6bd837150aa97608e110352065ea9ae60
Nalipp <nateplipp@gmail.com>
2017-09-30 22:02:07 -0700

    improve_algorithm

--- server/index.js
+++ server/index.js
@@ -1,34 +1,49 @@
+
 const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const request = require('request');
 
+require('dotenv').config()
+
 app.use(express.static(path.join(__dirname, '../')));
 
 app.get('/search', function(req, res, next) {
   address = req.query.address;
 
-  let apiKey = 'AIzaSyDlFlMD-AF24FF2uSZLO1qdXhqYqjOtmkU';
-request('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=' + apiKey, function (error, response, body) {
+  let apiKey = process.env.GOOGLE_API_KEY
+  request(
+    'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + apiKey, function (error, response, body) {
 
   let latInput = JSON.parse(body).results[0].geometry.location.lat;
   let lngInput = JSON.parse(body).results[0].geometry.location.lng;
 
-  let baseLat = String(latInput).slice(0, latInput.length - 2);
-  let baseLng = String(lngInput).slice(0, lngInput.length - 2);
+  let strLatInput = String(latInput);
+  let strLngInput = String(lngInput);
+
+  console.log(strLatInput);
+  console.log(strLngInput);
+
+  let baseLat = strLatInput.slice(0, strLatInput.length - 5);
+  let baseLng = strLngInput.slice(0, strLngInput.length - 5);
+
+  console.log(baseLat); 
+  console.log(baseLng); 
 
   let urlsArr = [];
 
   for (let i = 0; i < 20; i++) {
-    let randomize1 = Math.floor(Math.random() * (99 - 10) + 10);
-    let randomize2 = Math.floor(Math.random() * (99 - 10) + 10);
+    let randomize1 = Math.floor(Math.random() * (999 - 100) + 100);
+    let randomize2 = Math.floor(Math.random() * (999 - 100) + 100);
 
     let lat = baseLat + randomize1;
     let lng = baseLng + randomize2;
 
-    urlsArr.push('https://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + lat + ',' + lng + '&heading=151.78&pitch=-0.76&key=AIzaSyB-csukfcDPdI-2zc5geKecHwwl3atroA0');
+    urlsArr.push(
+      'https://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + lat + ',' + lng + '&heading=151.78&pitch=-0.76&key=AIzaSyB-csukfcDPdI-2zc5geKecHwwl3atroA0');
   }
+
   res.send(urlsArr);
   });
 });
@@ -36,3 +51,4 @@ request('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphithe
 app.listen(PORT, () => {
   console.log('NodeSchool is running on port', PORT);
 });
+
